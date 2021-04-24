6c759e1e8bb113e48c80dbd2aea0c4282d39dc82
Adam AKKARI <adam.akkari@epitech.eu>
2021-04-24 17:36:05 +0200

    Added block generation in Circuit scene

--- Circuit.gd
+++ Circuit.gd
@@ -2,14 +2,53 @@ extends Node2D
 
 signal tick
 
+enum BLOCKTYPE {
+	empty,
+	generator,
+	flowLeft,
+	flowUp,
+	flowRight,
+	flowDown
+}
+
+var block_scene = preload("res://entities/Block.tscn")
+
+# coordinates formula:
+# from X,Y to I:
+# x + y * width
+# from I to X,Y:
+# x = i % width
+# y = i / height
+var map = []
+export var width = 16
+export var height = 9
+
 func _ready():
-	pass # Replace with function body.
+	for i in range(width * height):
+		map.append(BLOCKTYPE.empty)
+	_add_block_at(BLOCKTYPE.generator, 1, 1)
+	_add_block_at(BLOCKTYPE.flowRight, 2, 1)
+	_add_block_at(BLOCKTYPE.flowDown, 3, 1)
+	_add_block_at(BLOCKTYPE.flowRight, 3, 2)
+	_add_block_at(BLOCKTYPE.flowRight, 4, 2)
+	
+	for j in map.size():
+		if map[j] != BLOCKTYPE.empty:
+			var block = block_scene.instance()
+			block.position = Vector2((j % width) * 32, (j / width) * 32)
+			block.block_type = map[j]
+			connect("tick", block, "_on_tick")
+			add_child(block)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func _add_block_at(block_type, x, y):
+	if (x + y * width) > (width * height):
+		pass
+	map[x + y * width] = block_type
 
 func _on_Timer_timeout():
 	emit_signal("tick")
---
+++ entities/Block.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+enum TYPE {
+	empty,
+	generator,
+	flowLeft,
+	flowUp,
+	flowRight,
+	flowDown
+}
+
+var current_flowing = false
+
+export var block_type = TYPE.generator
+
+onready var sprite = $Sprite
+onready var animplayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	match block_type:
+		TYPE.generator:
+			sprite.texture = preload("res://assets/images/block_generator.png")
+		TYPE.flowLeft:
+			sprite.texture = preload("res://assets/images/block_flow_left.png")
+		TYPE.flowUp:
+			sprite.texture = preload("res://assets/images/block_flow_up.png")
+		TYPE.flowRight:
+			sprite.texture = preload("res://assets/images/block_flow_right.png")
+		TYPE.flowDown:
+			sprite.texture = preload("res://assets/images/block_flow_down.png")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_tick():
+	if current_flowing:
+		current_flowing = false
+		animplayer.play("off")
+	else:
+		current_flowing = true
+		animplayer.play("on")
